b9267e2340716986a18c7e13dda654598f0c9d10
koz1tsk1y <koz1tsk1y@mail.ru>
2024-09-18 16:59:33 +0300

    Сделать баннер-слайдер

--- src/js/_banner-slider.js
+++ src/js/_banner-slider.js
@@ -1,22 +1,21 @@
 const bannerSliders = document.querySelectorAll('.banner-slider');
 
 if (bannerSliders.length > 0) {
-  
+
   bannerSliders.forEach(slider => {
-    const 
+    const
       slides = slider.querySelectorAll('.banner-slider__slide'),
       pagination = slider.querySelector('.banner-slider__pagination'),
       paginationItems = slider.querySelectorAll('.banner-slider__pagination-item');
     let
       currentSlide = 0;
-  
-  
+
     const resetClass = (els, className) => {
       els.forEach(el => {
         el.classList.remove(className);
       });
     }
-  
+
     const switchSlide = (index) => {
       if (slides.length > 0) {
         slides[currentSlide].classList.remove('banner-slider__slide--active');
@@ -26,10 +25,22 @@ if (bannerSliders.length > 0) {
           paginationItems[index].classList.add('banner-slider__pagination-item--active');
         }
         slider.style.height = `${slides[index].offsetHeight}px`;
+
+        // Получаем .banner-slider__slide-column активного слайда и его нижнюю точку
+        const activeSlideColumn = slides[index].querySelector('.banner-slider__slide-column');
+        if (activeSlideColumn) {
+          const columnHeight = activeSlideColumn.offsetHeight;
+          const slidePaddingTop = parseFloat(window.getComputedStyle(slides[index]).paddingTop);
+          pagination.style.top = `${slidePaddingTop + columnHeight}px`;
+        }
+
+        // Выводим внутренний отступ сверху текущего слайда
+        const slidePaddingTop = window.getComputedStyle(slides[index]).paddingTop;
+        console.log(`Внутренний отступ сверху текущего слайда: ${slidePaddingTop}`);
       }
       currentSlide = index;
     }
-  
+
     if (slides.length > 0) {
       resetClass(slides, 'banner-slider__slide--active');
       if (slides.length == paginationItems.length) {
@@ -42,7 +53,7 @@ if (bannerSliders.length > 0) {
       }
       switchSlide(currentSlide);
     }
-  
+
     resetClass(slides, 'banner-slider__slide--active');
     slides[currentSlide].classList.add('banner-slider__slide--active');
   });
