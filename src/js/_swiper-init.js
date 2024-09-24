0c84791591afc01cad01b711f3d8a7c451c77ca6
koz1tsk1y <koz1tsk1y@mail.ru>
2024-09-24 13:17:57 +0300

    Сверстать страницу Юридическое сопровождение

--- src/js/_swiper-init.js
+++ src/js/_swiper-init.js
@@ -1,4 +1,4 @@
-new Swiper('.swiper:not(.swiper--3, .portfolio)', {
+new Swiper('.swiper:not(.swiper--3, .portfolio, .tariffs)', {
   slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination',
@@ -48,4 +48,29 @@ new Swiper('.portfolio', {
     prevEl: '.swiper-btn-prev',
   },
   speed: 500,
+});
+
+new Swiper('.tariffs', {
+  slidesPerView: 1,
+  autoHeight: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  speed: 500,
+
+  breakpoints: {
+    376: {
+      slidesPerView: 1.3,
+    },
+    481: {
+      slidesPerView: 1.5,
+    },
+    769: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    }
+  }
 });
